b019305b896ac83936791ef9c8751d7a2004aff5
derpyhooves <56843137+scottoss@users.noreply.github.com>
2022-02-16 20:26:06 +0100

    api update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const LIBRARY_VERSION = "5.2.7";
 
 export const defaultConfig = {
-    apiURL: "https://api.revolt.chat",
+    apiURL: "https://api.bronycord.cf:2096",
     autoReconnect: true,
     heartbeat: 30,
     debug: false,
